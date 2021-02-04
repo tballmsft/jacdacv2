711c5c43cea64bf7524b651c631004771f3cf41e
Tom Ball <tballmsft@users.noreply.github.com>
2021-02-03 16:50:47 -0800

    more on buttons

--- main.ts
+++ main.ts
@@ -1,10 +1,14 @@
 function startAll() {
     const accel = new jacdac.microbitAccelerometer();
-    let buttonA = new jacdac.microbitButton("buttonA", EventBusSource.MICROBIT_ID_BUTTON_A);
-    let buttonB = new jacdac.microbitButton("buttonB", EventBusSource.MICROBIT_ID_BUTTON_B);
+    let buttonA = new jacdac.microbitButton("buttonA", Button.A);
+    let buttonB = new jacdac.microbitButton("buttonB", Button.B);
+    let buttonAB = new jacdac.microbitButton("buttonAB", Button.AB);
     accel.start();
     buttonA.start();
     buttonB.start();
+    buttonAB.start();
 }
 
 startAll();
+
+// TODO: SCREEN, temperature, light detector, microphone, compass, ...
--- microbitButtons.ts
+++ microbitButtons.ts
@@ -35,9 +35,9 @@ namespace jacdac {
     }
 
     export class microbitButton extends jacdac.Host {
-        constructor(button: string, private microbitButton: number) {
-            super(button, SRV_BUTTON);
-            control.onEvent(microbitButton, EventBusValue.MICROBIT_EVT_ANY, 
+        constructor(dev: string, private button: Button) {
+            super(dev, SRV_BUTTON);
+            control.onEvent(button, EventBusValue.MICROBIT_EVT_ANY, 
             () => {    
                 let v = control.eventValue();    
                 if (v == EventBusValue.MICROBIT_BUTTON_EVT_DOWN)
@@ -45,6 +45,9 @@ namespace jacdac {
                 else if (v == EventBusValue.MICROBIT_BUTTON_EVT_UP)
                     this.sendEvent(ButtonEvent.Up);
             })
+        }
+        handlePacket(packet: JDPacket) {
+            this.handleRegBool(packet, ButtonReg.Pressed, input.buttonIsPressed(this.button));
         }
     }
 }
