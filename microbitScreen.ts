8f12faaf07d63fa8573bed3961933db904c062ed
Tom Ball <tballmsft@users.noreply.github.com>
2021-02-04 16:00:31 -0800

    columns are byte-aligned

--- microbitScreen.ts
+++ microbitScreen.ts
@@ -47,8 +47,8 @@ namespace microbit {
                 if (packet.is_reg_set) {
                     let x = 0, y = 0;
                     for (let i=0; i<25; i++) {
-                        let byte = i >> 3;
-                        let bit = 1 << (i - (byte << 3)); 
+                        let byte = Math.floor(i/5);
+                        let bit = 1 << (i - (byte*5)); 
                         if (packet.data[byte] & bit) 
                             led.plot(x,y);
                         else led.unplot(x,y);
@@ -56,11 +56,11 @@ namespace microbit {
                     }
                 } else {
                     let x = 0, y = 0;
-                    let buf = Buffer.create(4);
+                    let buf = Buffer.create(5);
                     for (let i=0; i<25; i++) {
                         if (led.point(x, y)) {
-                            let byte = i >> 3;
-                            let bit = 1 << (i - (byte << 3)); 
+                            let byte = Math.floor(i/5);
+                            let bit = 1 << (i - (byte*5)); 
                             packet.data[byte] |= bit; 
                         }
                         x++; if (x == 5) { x=0; y++; }
